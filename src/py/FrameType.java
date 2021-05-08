0f3ca34c143b5d2f59c9be74bd721fe52c7c866c
towzeur <towzeur@gmail.com>
2021-05-08 21:06:28 +0200

    FrameType

--- src/py/FrameType.java
+++
@@ -1,8 +0,0 @@
-package com.codingame.game;
-
-public enum FrameType {
-    GATHERING,
-    ACTIONS,
-    SUN_MOVE,
-    INIT
-}
---
+++ src/py/FrameType.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class FrameType(Enum):
+    GATHERING = 1
+    ACTIONS   = 2
+    SUN_MOVE  = 3
+    INIT      = 4
+
