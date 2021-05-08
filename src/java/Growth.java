0fcc68af1669fbec7893b1c833c4844d82f9bd1c
towzeur <towzeur@gmail.com>
2021-05-09 01:38:20 +0200

    remove src/java

--- src/java/Growth.java
+++
@@ -1,7 +0,0 @@
-package com.codingame.game;
-
-public class Growth {
-    public int targetId;
-    public Player player;
-    public int cost;
-}
--- src/java/action/WaitAction.java
+++
@@ -1,9 +0,0 @@
-package com.codingame.game.action;
-
-public class WaitAction extends Action {
-
-    @Override
-    public boolean isWait() {
-        return true;
-    }
-}
