25d5a2e15a891b8478e44a41331b2bb0516181b7
towzeur <towzeur@gmail.com>
2021-05-08 20:44:33 +0200

    Player

--- src/py/main.py
+++ src/py/main.py
@@ -1,9 +1,14 @@
 #from action.WaitAction import WaitAction
 from Sun import Sun
-
-
 s = Sun()
 print(s.getOrientation())
 s.setOrientation(4)
 print(s.getOrientation())
+
+
+
+from Seed import Seed
+s = Seed()
+
+#from Tree import Tree
 
