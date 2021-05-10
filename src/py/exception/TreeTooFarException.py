5bf8ebd6886308afa900a1b995b324e7b7df3279
towzeur <towzeur@gmail.com>
2021-05-11 01:43:08 +0200

    changed 'from' variable name

--- src/py/exception/TreeTooFarException.py
+++ src/py/exception/TreeTooFarException.py
@@ -1,9 +1,8 @@
 from .GameException import GameException
 
+
 class TreeTooFarException(GameException):
-
-    def __init__(self, from: int, to: int):
-        super().__init__(f"The tree on cell {from} is too far from cell {to} to plant a seed there")
-
-    
-
+    def __init__(self, from_cell: int, to_cell: int):
+        super().__init__(
+            f"The tree on cell {from_cell} is too far from cell {to_cell} to plant a seed there"
+        )
