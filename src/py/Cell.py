787b76a9234d9e96047acff57f71b9a738354123
towzeur <towzeur@gmail.com>
2021-05-11 01:42:28 +0200

    python-static-class-attribute-of-the-class-itself

--- src/py/Cell.py
+++ src/py/Cell.py
@@ -1,28 +1,23 @@
-
 class Cell:
 
-    NO_CELL = Cell(-1, valid=False) # : 'Cell' =
-        
+    NO_CELL = None
+
     def __init__(self, index: int, valid: bool = True):
         self._valid = valid
-        self._richness : int = None
-        self._index : int = index
-    
+        self._richness: int = None
+        self._index: int = index
 
     def getIndex(self) -> int:
         return self._index if self._valid else -1
-    
 
     def isValid(self) -> bool:
         return self._valid
-    
 
     def setRichness(self, richness: int):
         self._richness = richness
-    
 
     def getRichness(self) -> int:
         return self._richness
-    
 
 
+Cell.NO_CELL = Cell(-1, valid=False)
